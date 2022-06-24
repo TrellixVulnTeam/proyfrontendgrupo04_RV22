2562d3c48d4c9b47ac54c7fb50d798dcc8fdccc3
jona <jevkjev@gmail.com>
2022-06-23 23:22:19 -0300

    plantilla alta empleado y recurso

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AltaEmpleadoComponent } from './components/form/alta-empleado/alta-empleado.component';
+import { AltaReunionComponent } from './components/form/alta-reunion/alta-reunion.component';
 import { EmpleadoComponent } from './components/empleado/empleado.component';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-    //cuando no ingrese nada va al login
-    {path: '', redirectTo:'login',pathMatch:'full'}, 
-    //Las rutas validas
-    {path: 'login', component: LoginComponent}, 
-    {path: 'empleadosRegistrados', component:EmpleadoComponent},
-   //cuando se ingresa cualquier otra ruta volvera al login
-    {path: '**', redirectTo:'login',pathMatch:'full'},
+  //cuando no ingrese nada va al login
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  //Las rutas validas
+  { path: 'login', component: LoginComponent },
+  { path: 'empleadosRegistrados', component: EmpleadoComponent },
+  //cuando se ingresa cualquier otra ruta volvera al login
+  { path: '**', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'alta-empleado', component: AltaEmpleadoComponent },
+  { path: 'alta-reunion', component: AltaReunionComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -7,10 +7,13 @@ import { FooterComponent } from './components/layout/footer/footer.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { LoginComponent } from './components/login/login.component';
 import { FormsModule } from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { LoginService } from './services/login.service';
 import { EmpleadoComponent } from './components/empleado/empleado.component';
-
+import { AltaEmpleadoComponent } from './components/form/alta-empleado/alta-empleado.component';
+import { AltaReunionComponent } from './components/form/alta-reunion/alta-reunion.component';
+import { FormRecursosComponent } from './components/form/form-recursos/form-recursos.component';
+import { FormParticipantesComponent } from './components/form/form-participantes/form-participantes.component';
 
 @NgModule({
   declarations: [
@@ -18,16 +21,19 @@ import { EmpleadoComponent } from './components/empleado/empleado.component';
     FooterComponent,
     HeaderComponent,
     LoginComponent,
-    EmpleadoComponent
+    EmpleadoComponent,
+    AltaEmpleadoComponent,
+    AltaReunionComponent,
+    FormRecursosComponent,
+    FormParticipantesComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule, // cliente
-    FormsModule,  //para los formularios
-
+    FormsModule, //para los formularios
   ],
   providers: [LoginService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

fa2f08b17de0e8887b20ce423d7904caaf5407d2
jona <jevkjev@gmail.com>
2022-06-30 19:36:57 -0300

    correccion en service.ts

--- Frontend/src/app/services/calendargoogle.service.ts
+++ Frontend/src/app/services/calendargoogle.service.ts
@@ -6,22 +6,22 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class CalendargoogleService {
-  apiKey = ''; //token generado para autorizar uso de la api
-  idCalendar = ''; //id del calendadario creado
+  apiKey = ""; //token generado para autorizar uso de la api
+  idCalendar = ""; //id del calendadario creado
   urlGoogle = 'https://www.googleapis.com/calendar/v3/calendars/';
-  constructor(private _http: HttpClient) {}
+  constructor(private _http: HttpClient) { }
   createEvent(event: any): Observable<any> {
     const httpOptions = {
       headers: new HttpHeaders({
-        Authorization: 'Bearet ' + this.apiKey,
-        'Content-Type': 'application/json',
+        "Authorization": "Bearet " + this.apiKey,
+        "Content-Type": "application/json",
       }),
       params: new HttpParams({}),
     };
     let body = JSON.stringify(event);
     //console.log(body);
     return this._http.post(
-      this.urlGoogle + this.idCalendar + '/event',
+      this.urlGoogle + this.idCalendar + "/event",
       body,
       httpOptions
     );

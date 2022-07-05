4507fc5afd34a8d01650db30cf22156fc84c9b33
IvanS4L4S <shibusen12345@gmail.com>
2022-07-05 13:54:46 -0300

    se dio estilo y se se genero el pfd y qr de reunion

--- Frontend/src/app/components/reunion-pdf/reunion-pdf.component.ts
+++ Frontend/src/app/components/reunion-pdf/reunion-pdf.component.ts
@@ -5,32 +5,34 @@ import { ReunionService } from 'src/app/services/reunion.service';
 import { NgxQrcodeElementTypes, NgxQrcodeErrorCorrectionLevels } from '@techiediaries/ngx-qrcode';
 import * as printJS from 'print-js';
 
+import html2canvas from 'html2canvas';
+import jspdf from 'jspdf';
+
+
 @Component({
   selector: 'app-reunion-pdf',
   templateUrl: './reunion-pdf.component.html',
   styleUrls: ['./reunion-pdf.component.css']
 })
 export class ReunionPdfComponent implements OnInit {
-
   accion!:string;
   url!:string;
   constructor(private activateRoute:ActivatedRoute, private reunionService: ReunionService) {   }
   reunion!:Reunion;
   ngOnInit(): void {
     this.activateRoute.params.subscribe(params =>{
-      
       if(params['id'] == '0'){
         this.accion="new";
-     
       }
       else{
         this.buscarReunionID(params['id']);
-        this.url = "localhost:4200/reunionpdf/"+params['id'];
-        //this.getPersonas();
+        this.url = 'http://localhost:4200/reunionpdf/'+params['id'];
       }
     }) 
-  
   }
+
+
+  
   buscarReunionID(reunionID:string)
   {
     this.reunion= new Reunion();
@@ -44,27 +46,34 @@ export class ReunionPdfComponent implements OnInit {
   }
 
 //******************************** Generar QR ********************************
-    title = 'qr-code';
 
-    ulr= this.url;
-    profile='routeToMyProfile';
-    elementType=NgxQrcodeElementTypes.URL;
-    errorCorrectionLevel=NgxQrcodeErrorCorrectionLevels.HIGH;
-    value=this.ulr+this.profile;
-
+title = 'qr-code'; 
+ulr= '';
+profile='routeToMyProfile';
+elementType=NgxQrcodeElementTypes.URL;
+errorCorrectionLevel=NgxQrcodeErrorCorrectionLevels.HIGH;
+value=this.ulr+this.profile;
 
      // ******************************** Generar PDF ********************************
-imprimir(){
-
-  printJS('document.URL','html');
-
- /*  printJS(
-    {
-      printable:'./reunion-pdf.component.html', 
-      type:'html',
-      style:'.special-element { visibility: visible; }'
-    }
-  ) */
+title1 = 'html-to-pdf-angular-application';
+public convetToPDF()
+{
+var data = document.getElementById('contentToConvert');
+html2canvas(data).then(canvas => {
+  // Few necessary setting options
+  var imgWidth = 208;
+  var pageHeight = 295;
+  var imgHeight = canvas.height * imgWidth / canvas.width;
+  var heightLeft = imgHeight;
+  
+  const contentDataURL = canvas.toDataURL('image/png')
+  let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF
+  var position = 0;
+  pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
+  pdf.save('new-file.pdf'); // Generated PDF
+  });
 }
 
 }
+
+

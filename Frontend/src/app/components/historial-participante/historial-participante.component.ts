4df53e8f230c638a3c78dddab47f28b0527e8958
IvanS4L4S <shibusen12345@gmail.com>
2022-07-08 15:24:50 -0300

    se creo la estructura del filtro

--- Frontend/src/app/components/historial-participante/historial-participante.component.ts
+++ Frontend/src/app/components/historial-participante/historial-participante.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { id } from 'date-fns/locale';
 import { Reunion } from 'src/app/models/reunion';
+import { LoginService } from 'src/app/services/login.service';
 import { ReunionService } from 'src/app/services/reunion.service';
 
 @Component({
@@ -10,20 +11,40 @@ import { ReunionService } from 'src/app/services/reunion.service';
 })
 export class HistorialParticipanteComponent implements OnInit {
 
-  constructor(private reunionService:ReunionService) { }
-
+  constructor(private reunionService:ReunionService,private loginService:LoginService) { 
+    this.recuperarPasajes()
+  }
+  id:string=this.loginService.idLogged();
+  reunion!:Reunion;
   reuniones!:Array<Reunion>;
   ngOnInit(): void {
   }
 
 
-  getHistorialReuniones(id:string){
-    this.reunionService.getReunionParticipante(id).subscribe(
+/*   getHistorialReuniones(){
+    this.reunionService.getReunionParticipante('62bdfed340cc7ecd9dd03bea').subscribe(
       (result) =>{
       
         console.log(result);
       }      
     )
+  } */
+  recuperarPasajes(){
+    this.reunionService.getReunionParticipante('62c3bbe94401d30e30eba846').subscribe(
+      result=>{
+        this.reuniones= new Array<Reunion>();
+        result.forEach((element:any) => {
+          this.reunion=new Reunion();
+          Object.assign(this.reunion,element);
+         this.reuniones.push(this.reunion);       
+        });
+        console.log(this.loginService.idLogged());
+        console.log(result);
+      },
+      error=>{
+          console.log("error")
+      } 
+    );
   }
 
 }
